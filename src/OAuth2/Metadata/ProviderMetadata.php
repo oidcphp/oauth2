0ced56e46a273d5a77583fe0ef3db30e2dc5763a
MilesChou <jangconan@gmail.com>
2019-11-11 03:42:46 +0800

    Update provider metadata

--- src/OAuth2/Metadata/ProviderMetadata.php
+++ src/OAuth2/Metadata/ProviderMetadata.php
@@ -8,12 +8,30 @@ use OpenIDConnect\OAuth2\Traits\ParameterTrait;
 /**
  * OAuth 2.0 Authorization Server Metadata
  *
- * @method string authorizationEndpoint() URL of the authorization server's authorization endpoint.
- * @method string tokenEndpoint() URL of the authorization server's token endpoint.
- * @method string userinfoEndpoint() URL of the authorization server's UserInfo Endpoint
+ * @method string issuer()
+ * @method string authorizationEndpoint()
+ * @method string jwksUri()
+ * @method string registrationEndpoint()
+ * @method array scopesSupported()
+ * @method array responseTypesSupported()
+ * @method array responseModesSupported()
+ * @method array grantTypesSupported()
+ * @method string tokenEndpoint()
+ * @method array tokenEndpointAuthMethodsSupported()
+ * @method array tokenEndpointAuthSigningAlgValuesSupported()
+ * @method string revocationEndpoint()
+ * @method array revocationEndpointAuthMethodsSupported()
+ * @method array revocationEndpointAuthSigningAlgValuesSupported()
+ * @method string introspectionEndpoint()
+ * @method array introspectionEndpointAuthMethodsSupported()
+ * @method array introspectionEndpointAuthSigningAlgValuesSupported()
+ * @method array codeChallengeMethodsSupported() PKCE [RFC7636] code challenge methods supported
+ * @method string serviceDocumentation() URL of a page containing human-readable information for developer
+ * @method array uiLocalesSupported() Languages and scripts supported for the user interface
+ * @method string opPolicyUri() URL that how the client can use the data provided
+ * @method string opTosUri() URL that about the authorization server's terms of service
  *
  * @see https://tools.ietf.org/html/rfc8414#section-2
- * @see https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderMetadata
  */
 class ProviderMetadata implements JsonSerializable
 {
